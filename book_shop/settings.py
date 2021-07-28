3ba9094e9773ebdc1fe05b2b3ef28330528dd9bc
Your Name <isomiddnovdiyor@gmail.com>
2021-07-28 19:46:51 +0500

    first commit

--- book_shop/settings.py
+++ book_shop/settings.py
@@ -92,3 +92,20 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+# gmail_send/settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'isomiddnovdiyor@gmail.com'
+EMAIL_HOST_PASSWORD = 'Muhammaddiyor'
+
+ACCOUNT_ACTIVATION_DAYS = 7  # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # Automatically log the user in.
